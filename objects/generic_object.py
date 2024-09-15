858ce06b76f0cabd542a45f90db12a9e17dc855f
FKettl <fekettl@gmail.com>
2024-09-15 17:56:05 -0300

    add transformation

--- objects/generic_object.py
+++ objects/generic_object.py
@@ -38,31 +38,40 @@ class GenericObject(ABC):
         return np.matrix([[np.cos(angle), -np.sin(angle), 0], [np.sin(angle), np.cos(angle), 0], [0, 0, 1]])
 
     def translate(self, dx, dy):
-        translation_matrix = self.create_translation_matrix(dx, dy)
-        for x in self.coordinates:
-            point = np.matrix(x)
-            new_point = point * translation_matrix
-            x[0] = new_point.item(0)
-            x[1] = new_point.item(1)
+        return self.create_translation_matrix(dx, dy)
 
     def scale(self, sx, sy):
         scale_matrix = self.create_scale_matrix(sx, sy)
         translation_matrix_origin = self.create_translation_matrix(-self.center[0], -self.center[1])
         translation_matrix_return = self.create_translation_matrix(self.center[0], self.center[1])
-        scale_matrix = translation_matrix_origin * scale_matrix * translation_matrix_return
-        for x in self.coordinates:
-            point = np.matrix(x)
-            new_point = point * scale_matrix
-            x[0] = new_point.item(0)
-            x[1] = new_point.item(1)
+        scale_matrix = np.dot(translation_matrix_origin, scale_matrix)
+        scale_matrix = np.dot(scale_matrix, translation_matrix_return)
+        return scale_matrix
 
     def rotate(self, angle, center):
         rotation_matrix = self.create_rotation_matrix(angle)
         translation_matrix_origin = self.create_translation_matrix(-center[0], -center[1])
         translation_matrix_return = self.create_translation_matrix(center[0], center[1])
-        rotation_matrix = translation_matrix_origin * rotation_matrix * translation_matrix_return
+        rotation_matrix = np.dot(translation_matrix_origin, rotation_matrix)
+        rotation_matrix = np.dot(rotation_matrix, translation_matrix_return)
+        return rotation_matrix
+
+    def get_transformation_matrix(self, transformation):
+        if transformation[0] == "T":
+            return self.translate(transformation[1], transformation[2])
+        elif transformation[0] == "S":
+            return self.scale(transformation[1], transformation[2])
+        elif transformation[0] == "R":
+            return self.rotate(transformation[1])
+
+    def transform(self, transformations):
+        transformation_matrix = self.get_transformation_matrix(transformations[0])
+
+        for i in range(1, len(transformations)):
+            transformation_matrix = np.dot(transformation_matrix, self.get_transformation_matrix(transformations[i]))
+
         for x in self.coordinates:
             point = np.matrix(x)
-            new_point = point * rotation_matrix
+            new_point = np.dot(point, transformation_matrix)
             x[0] = new_point.item(0)
             x[1] = new_point.item(1)
