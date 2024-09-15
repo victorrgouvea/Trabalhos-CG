7428b0e85d60e9a181bc301768ddd38328164cea
FKettl <fekettl@gmail.com>
2024-09-14 21:17:02 -0300

    Add transformations

--- objects/generic_object.py
+++ objects/generic_object.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+import numpy as np
+
 
 class GenericObject(ABC):
 
@@ -25,3 +27,42 @@ class GenericObject(ABC):
             total_y += x[1]
 
         return (total_x / len(self.coordinates), total_y / len(self.coordinates))
+
+    def create_translation_matrix(self, dx, dy):
+        return np.matrix([[1, 0, dx], [0, 1, dy], [0, 0, 1]])
+
+    def create_scale_matrix(self, sx, sy):
+        return np.matrix([[sx, 0, 0], [0, sy, 0], [0, 0, 1]])
+
+    def create_rotation_matrix(self, angle):
+        return np.matrix([[np.cos(angle), -np.sin(angle), 0], [np.sin(angle), np.cos(angle), 0], [0, 0, 1]])
+
+    def translate(self, dx, dy):
+        translation_matrix = self.create_translation_matrix(dx, dy)
+        for x in self.coordinates:
+            point = np.matrix(x)
+            new_point = point * translation_matrix
+            x[0] = new_point.item(0)
+            x[1] = new_point.item(1)
+
+    def scale(self, sx, sy):
+        scale_matrix = self.create_scale_matrix(sx, sy)
+        translation_matrix_origin = self.create_translation_matrix(-self.center[0], -self.center[1])
+        translation_matrix_return = self.create_translation_matrix(self.center[0], self.center[1])
+        scale_matrix = translation_matrix_origin * scale_matrix * translation_matrix_return
+        for x in self.coordinates:
+            point = np.matrix(x)
+            new_point = point * scale_matrix
+            x[0] = new_point.item(0)
+            x[1] = new_point.item(1)
+
+    def rotate(self, angle, center):
+        rotation_matrix = self.create_rotation_matrix(angle)
+        translation_matrix_origin = self.create_translation_matrix(-center[0], -center[1])
+        translation_matrix_return = self.create_translation_matrix(center[0], center[1])
+        rotation_matrix = translation_matrix_origin * rotation_matrix * translation_matrix_return
+        for x in self.coordinates:
+            point = np.matrix(x)
+            new_point = point * rotation_matrix
+            x[0] = new_point.item(0)
+            x[1] = new_point.item(1)
--- system/view_port.py
+++ system/view_port.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 class ViewPort:
 
     def __init__(self, vpx, vpy, window) -> None:
@@ -16,12 +13,3 @@ class ViewPort:
         vp_x = (((x - wcmin[0]) / (wcmax[0] - wcmin[0])) * (self.vpxmax - self.vpxmin))
         vp_y = ((1 - ((y - wcmin[1]) / (wcmax[1] - wcmin[1]))) * (self.vpymax - self.vpymin))
         return (vp_x, vp_y)
-
-    def create_translation_matrix(self, dx, dy):
-        return np.matrix([[1, 0, dx], [0, 1, dy], [0, 0, 1]])
-
-    def create_scale_matrix(self, sx, sy):
-        return np.matrix([[sx, 0, 0], [0, sy, 0], [0, 0, 1]])
-
-    def create_rotation_matrix(self, angle):
-        return np.matrix([[np.cos(angle), -np.sin(angle), 0], [np.sin(angle), np.cos(angle), 0], [0, 0, 1]])
