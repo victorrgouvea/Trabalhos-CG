8de42f4ac19203e01861000a680d0d8f47ef4148
FKettl <fekettl@gmail.com>
2024-09-14 21:03:02 -0300

    Add center, Homogeneous coordinates and base matrix creation

--- objects/generic_object.py
+++ objects/generic_object.py
@@ -7,10 +7,21 @@ class GenericObject(ABC):
         self.type = type
         self.coordinates = []
         for x in coordinates:
-            self.coordinates.append(x)
+            self.coordinates.append([x[0], x[1], 1])
         self.coordinates = coordinates
         self.color = color
-    
+        self.center = self.get_center()
+
     @abstractmethod
     def draw(self, context, viewport_functionf):
         pass
+
+    def get_center(self):
+        total_x = 0
+        total_y = 0
+
+        for x in self.coordinates:
+            total_x += x[0]
+            total_y += x[1]
+
+        return (total_x / len(self.coordinates), total_y / len(self.coordinates))
