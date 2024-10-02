57c32f6d04f97ba610dea6f469905d7bcdf01396
FKettl <felipekettl@onsign.tv>
2024-10-02 17:34:59 -0300

    initial view fix

--- system/view_port.py
+++ system/view_port.py
@@ -10,6 +10,8 @@ class ViewPort:
     def transform(self, x, y):
         wcmax =  [1, 1]
         wcmin = [-1, -1]
+        x += 0.3
         vp_x = (((x - wcmin[0]) / (wcmax[0] - wcmin[0])) * (self.vpxmax - self.vpxmin))
         vp_y = ((1 - ((y - wcmin[1]) / (wcmax[1] - wcmin[1]))) * (self.vpymax - self.vpymin))
+        #vp_x += (self.vpxmax - self.vpxmin)/8
         return [vp_x, vp_y]
